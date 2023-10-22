40914fef9c70a3144544a87452061a2229877d16
pkrawat1 <pankajrawat19sept@gmail.com>
2023-10-22 11:36:14 +0530

    wip testing

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule TradeGalleon.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: [
+        "test.watch": :test
+      ]
     ]
   end
 
@@ -23,7 +26,9 @@ defmodule TradeGalleon.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:tesla, "~> 1.4"}
+      {:tesla, "~> 1.4"},
+      {:jason, "~> 1.4"},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 end
